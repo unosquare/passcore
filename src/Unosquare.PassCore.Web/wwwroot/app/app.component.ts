c82cf248d372a384b1c28a6d46a383348d0867f1
Israel <arnulfo_israel@hotmail.com>
2017-09-29 13:52:04 -0500

    First files

---
+++ src/Unosquare.PassCore.Web/wwwroot/app/app.component.ts
@@ -0,0 +1,10 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-root',
+    templateUrl: '',
+    styleUrls: ['']
+})
+export class AppComponent {
+    title = 'app';
+}
