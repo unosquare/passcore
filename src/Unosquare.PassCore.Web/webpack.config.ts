027d023728f5b2c48e8aa623ab00545fb0002a3a
chubbsnes65 <nestor.soto@unosquare.com>
2017-09-29 13:04:35 -0500

    Webpack instalation

---
+++ src/Unosquare.PassCore.Web/webpack.config.ts
@@ -0,0 +1,12 @@
+﻿import * as webpack from 'webpack';
+import * as path from 'path';
+
+const config: webpack.Configuration = {
+    entry: { foo:'./foo.js'},
+    output: {
+        path: '../wwwroot/scripts',
+        filename: 'app.min.js'
+    }
+};
+
+export default config;
