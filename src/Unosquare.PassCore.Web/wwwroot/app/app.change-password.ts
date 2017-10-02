2244c9726e032257955a197b5d18a624b244cae2
Israel <arnulfo_israel@hotmail.com>
2017-10-02 14:32:21 -0500

    moved view

--- src/Unosquare.PassCore.Web/wwwroot/app/app.change-password.ts
+++ src/Unosquare.PassCore.Web/wwwroot/app/app.change-password.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.change-password.html'
+  templateUrl: '../views/app.change-password.html'
 })
 export class ChangePasswordComponent {
   title = 'Change Password';
