adda71b70905e67920aff298972344ed93231b9a
Brian Martin <30876068+bmartin13@users.noreply.github.com>
2018-01-19 16:18:20 -0600

    Added checking for restricted group membership and some logging to stdout to track the change process.

--- src/Unosquare.PassCore.Web/Controllers/PasswordController.cs
+++ src/Unosquare.PassCore.Web/Controllers/PasswordController.cs
@@ -114,37 +114,26 @@
                         
                         return BadRequest(result);
                     }
-                    else
-                    {
-                        Console.WriteLine("Attempting to change password for: " + userPrincipal.Name);
-                    }
                     
                     // Check if password change is allowed
                     if (userPrincipal.UserCannotChangePassword)
                     {
-                        Console.WriteLine("ERROR: " + userPrincipal.Name + " is not allowed to change their password");
                         throw new Exception(_options.ClientSettings.Alerts.ErrorPasswordChangeNotAllowed);
                     }
 
                     // Validate user credentials
                     if (principalContext.ValidateCredentials(model.Username, model.CurrentPassword) == false)
                     {
-                        Console.WriteLine("ERROR: " + userPrincipal.Name + " entered invalid current credentials.");
                         throw new Exception(_options.ClientSettings.Alerts.ErrorInvalidCredentials);
                     }
 
                     // Verify user is not a member of an excluded group
                     if (_options.ClientSettings.CheckRestrictedAdGroups)
                     {
-                        Console.WriteLine("Checking for restricted group membership.");
-
                         foreach (Principal userPrincipalAuthGroup in userPrincipal.GetAuthorizationGroups())
                         {
-                            Console.WriteLine(userPrincipal.Name + " is a member of " + userPrincipalAuthGroup.Name + ".");
-
                             if (_options.ClientSettings.RestrictedADGroups.Contains(userPrincipalAuthGroup.Name))
                             {
-                                Console.WriteLine("ERROR: " + userPrincipal.Name + " is a member of " + userPrincipalAuthGroup.Name + "!");
                                 throw new Exception(_options.ClientSettings.Alerts.ErrorPasswordChangeNotAllowed);
                             }
                         }
@@ -155,7 +144,6 @@
                     {
                         // Try by regular ChangePassword method
                         userPrincipal.ChangePassword(model.CurrentPassword, model.NewPassword);
-                        Console.WriteLine("SUCCESS: " + userPrincipal.Name + " has changed their password.");
                     }
                     catch (Exception ex2)
                     {
@@ -163,7 +151,6 @@
                         if (_options.PasswordChangeOptions.UseAutomaticContext == false)
                         {
                             userPrincipal.SetPassword(model.NewPassword);
-                            Console.WriteLine("SUCCESS: " + userPrincipal.Name + " has changed their password.");
                         }
                         else
                         {
