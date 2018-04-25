872fc2d9371f7deb542533163a9a6c23d7e0e236
Israel <arnulfo_israel@hotmail.com>
2018-04-25 17:08:43 -0500

    updating webpack.config.js and package.json, missing aspnetcore package in csproj

--- src/Unosquare.PassCore.Web/webpack.config.js
+++ src/Unosquare.PassCore.Web/webpack.config.js
@@ -1,32 +1,39 @@
-﻿const { CommonsChunkPlugin } = require('webpack').optimize;
+﻿const { CommonsChunkPlugin } = require('webpack').optimize
 const { SourceMapDevToolPlugin } = require('webpack');
-const { AotPlugin } = require('@ngtools/webpack');
 
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const WebpackCleanupPlugin = require('webpack-cleanup-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const WebpackCleanupPlugin = require('webpack-cleanup-plugin')
 
-const path = require('path');
+const webpack = require('webpack')
+const path = require('path')
 
 module.exports = {
+    devtool: 'source-map',
+    devServer: {
+        historyApiFallback: true
+    },
     resolve: {
-        extensions: ['.ts', '.js'],
+        extensions: ['.ts', '.js', '.css']
     },
     entry: {
-        'polyfills': './ClientApp/polyfills.ts',
         'main': './ClientApp/main.ts',
-        'vendor': './ClientApp/vendor.ts'
+        'vendor': './ClientApp/vendor.ts',
+        'polyfills': './ClientApp/polyfills.ts'
     },
     output: {
         path: path.join(process.cwd(), './wwwroot'),
         filename: '[name].bundle.js',
-        chunkFilename: '[id].chunk.js'
+        chunkFilename: '[id].chunk.js',
     },
     module: {
         rules: [
-            { test: /\.ts$/, loader: '@ngtools/webpack'  },
             { test: /\.html$/, loader: 'raw-loader' },
             { test: /\.css$/, loader: 'raw-loader' },
+            {
+                test: /\.ts$/,
+                loaders: ['awesome-typescript-loader?useBabel=false', 'angular2-template-loader']
+            }
         ]
     },
     plugins: [
@@ -37,17 +44,23 @@ module.exports = {
         new HtmlWebpackPlugin({
             template: './ClientApp/index.html'
         }),
+        new webpack.ContextReplacementPlugin(
+            // The (\\|\/) piece accounts for path separators in *nix and Windows
+            /angular(\\|\/)core(\\|\/)@angular/,
+            './ClientApp', // location of your src
+            {} // a map of your routes
+        ),
+        new webpack.ContextReplacementPlugin(
+            // The (\\|\/) piece accounts for path separators in *nix and Windows
+            /@angular(\\|\/)core(\\|\/)esm5/,
+            './ClientApp', // location of your src
+            {} // a map of your routes
+        ),
         new SourceMapDevToolPlugin({
-            "filename": "[file].map[query]",
-            "moduleFilenameTemplate": "[resource-path]",
-            "fallbackModuleFilenameTemplate": "[resource-path]?[hash]",
-            "sourceRoot": "webpack:///"
-        }),
-        new AotPlugin({
-            'mainPath': 'main.ts',
-            'replaceExport': false,
-            'tsConfigPath': 'ClientApp\\tsconfig.app.json',
-            'skipCodeGeneration': true
+            "filename": '[file].map[query]',
+            "moduleFilenameTemplate": '[resource-path]',
+            "fallbackModuleFilenameTemplate": '[resource-path]?[hash]',
+            "sourceRoot": 'webpack:///'
         }),
         new CopyWebpackPlugin([{
             'context': 'ClientApp',
@@ -68,23 +81,15 @@ module.exports = {
             'context': 'ClientApp',
             'to': '',
             'from': {
-                'glob': 'favicon.ico',
-                'dot': true
-            }
-        },
-        {
-            'context': 'ClientApp',
-            'to': '',
-            'from': {
                 'glob': 'web.config',
                 'dot': true
             }
         }
         ], {
-            'ignore': [
-                '.gitkeep'
-            ],
-            'debug': 'warning'
-        }),
+                'ignore': [
+                    '.gitkeep'
+                ],
+                'debug': 'warning'
+            })
     ]
 }
