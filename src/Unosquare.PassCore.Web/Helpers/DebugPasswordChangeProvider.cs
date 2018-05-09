58058e7482985c6aa868100a50579ec0958bcc06
Michael Adams <unquietwiki@gmail.com>
2018-05-09 11:58:15 -0700

    Saw a way to fix the Codacy error

--- src/Unosquare.PassCore.Web/Helpers/DebugPasswordChangeProvider.cs
+++ src/Unosquare.PassCore.Web/Helpers/DebugPasswordChangeProvider.cs
@@ -3,16 +3,16 @@
     using System.Collections.Generic;
     using Unosquare.PassCore.Web.Models;
 
+    // Codacy kept thinking the original implementation was an issue.
+    // https://stackoverflow.com/questions/9415257/how-can-i-implement-static-methods-on-an-interface
+
     internal class DebugPasswordChangeProvider : IPasswordChangeProvider
     {
-        public ApiErrorItem PerformPasswordChange(ChangePasswordModel model)
+        static ApiErrorItem PerformPasswordChange(ChangePasswordModel model)
         {
             var username = model.Username.Substring(0, model.Username.IndexOf("@"));
-
             switch (username)
             {
-                case "pass":
-                    return null;
                 case "error":
                     return new ApiErrorItem { ErrorType = ApiErrorType.GeneralFailure, ErrorCode = ApiErrorCode.Generic, Message = "Error" };
                 case "notfound":
@@ -20,6 +20,10 @@
                 default:
                     return null;
             }
+        }
+        ApiErrorItem IPasswordChangeProvider.PerformPasswordChange(ChangePasswordModel model)
+        {
+            return DebugPasswordChangeProvider.PerformPasswordChange(model);
         }
     }
 }
