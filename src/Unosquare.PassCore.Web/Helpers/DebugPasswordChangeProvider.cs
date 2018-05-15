24bf56d10f09720f70475a31cd36cde4e5c7107b
Geovanni Perez <geovanni.perez@gmail.com>
2018-05-15 12:20:55 -0500

    Update DebugPasswordChangeProvider.cs

--- src/Unosquare.PassCore.Web/Helpers/DebugPasswordChangeProvider.cs
+++ src/Unosquare.PassCore.Web/Helpers/DebugPasswordChangeProvider.cs
@@ -8,13 +8,6 @@
     // Sonar-Codacy thought we needed a static method here; and suggested dual default nulls was pointless.
     internal class DebugPasswordChangeProvider : IPasswordChangeProvider
     {
-        private readonly AppSettings _options;
-
-        public DebugPasswordChangeProvider(IOptions<AppSettings> options)
-        {
-            _options = options.Value;
-        }
-
         public ApiErrorItem PerformPasswordChange(string username, string currentPassword, string newPassword)
         {
             var currentUsername = username.Substring(0, username.IndexOf("@", StringComparison.Ordinal));
