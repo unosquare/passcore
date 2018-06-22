c1c3bd57058640e282be6e3239e482b95e746161
Israel <arnulfo_israel@hotmail.com>
2018-06-22 13:19:17 -0500

    IIS website to get path and reset location

--- IISWebsite.ps1
+++ IISWebsite.ps1
@@ -1,42 +1,51 @@
-﻿echo "Configuring IIS"
+﻿Write-Host "Configuring IIS"
+
+$currentDirectory = (Get-Item -Path ".\").FullName
 
 $iisAppName = "passcore"
-
 $iisAppPoolName = "Passcore Pool PS"
 $iisAppPoolDotNetVersion = ""
 $iisAppPoolStartMode = "AlwaysRunning"
-
 #the directory where you will be serving the website from
-$directoryPath = "C:\passcorePS"
+$directoryPath = $args[0]
 
-Import-Module WebAdministration
+try {
+    Import-Module WebAdministration
 
-#navigate to the app pools root
-cd IIS:\AppPools\
-
-#Get-IISAppPool
-#Get-ItemProperty IIS:\AppPools\DefaultAPpPool | select cpu.limitInterval
-
-#check if the app pool exists
-if (!(Test-Path $iisAppPoolName -pathType container))
-{
-    echo "Creating $($iisAppPoolName)"
-    #create the app pool
-    $appPool = New-Item $iisAppPoolName
-    $appPool | Set-ItemProperty -Name "managedRuntimeVersion" -Value $iisAppPoolDotNetVersion
-    $appPool | Set-ItemProperty -Name "startMode" -value $iisAppPoolStartMode
-} 
-
-#navigate to the sites root
-cd IIS:\Sites\
-
-#check if the site exists
-if (Test-Path $iisAppName -pathType container)
-{
-    return echo "The site $($iisAppName) already exist"
+    #navigate to the app pools root
+    cd IIS:\AppPools\
+    
+    #check if the app pool exists
+    if (!(Test-Path $iisAppPoolName -pathType container))
+    {
+        Write-Host "Creating $($iisAppPoolName)"
+        #create the app pool
+        $appPool = New-Item $iisAppPoolName
+        $appPool | Set-ItemProperty -Name "managedRuntimeVersion" -Value $iisAppPoolDotNetVersion
+        $appPool | Set-ItemProperty -Name "startMode" -value $iisAppPoolStartMode
+    } 
+    
+    #navigate to the sites root
+    Set-Location IIS:\Sites\
+    
+    #check if the site exists
+    if (Test-Path $iisAppName -pathType container)
+    {
+        Write-Host "The site $($iisAppName) already exist"
+        exit 1
+    }
+    
+    Write-Host "Creating $($iisAppName) on IIS"
+    #create the site
+    $iisApp = New-Item $iisAppName -bindings @{protocol="http";bindingInformation="*:9091:"} -physicalPath $directoryPath
+    $iisApp | Set-ItemProperty -Name "applicationPool" -Value $iisAppPoolName
+    
+    $request = Invoke-WebRequest -Uri "http://localhost"
+    if ($request.StatusCode -ne 200) {
+        Write-Host "HTTP Error"
+        exit 1
+    }
 }
-
-echo "Creating the $($iisAppName) on IIS"
-#create the site
-$iisApp = New-Item $iisAppName -bindings @{protocol="http";bindingInformation="*:9091:"} -physicalPath $directoryPath
-$iisApp | Set-ItemProperty -Name "applicationPool" -Value $iisAppPoolName
+finally {
+    Set-Location $currentDirectory
+}
