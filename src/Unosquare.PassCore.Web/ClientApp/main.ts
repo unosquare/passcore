475e42f07ca6a34927c0ee32e22d0ec6184e45f4
Israel <arnulfo_israel@hotmail.com>
2018-08-09 17:12:01 -0500

    fetch settings before bootstrap the app to set language

--- src/Unosquare.PassCore.Web/ClientApp/main.ts
+++ src/Unosquare.PassCore.Web/ClientApp/main.ts
@@ -3,5 +3,8 @@ import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 enableProdMode();
-platformBrowserDynamic().bootstrapModule(AppModule)
+fetch('api/password').then(response => response.json()).then(config => {
+  window['lang'] = config.recaptcha.languageCode;
+  platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.log(err));
+});
