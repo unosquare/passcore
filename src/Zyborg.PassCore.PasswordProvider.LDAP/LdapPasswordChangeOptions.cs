e3032a9db839148e969970f736bb219f9bb9b836
Eugene Bekker <ebekker@users.noreply.github.com>
2018-08-10 13:25:06 -0400

    Adding changes to implement pure LDAP provider (#196)
    * adding a pure LDAP-based Password Change Provider
    
    * woops!
    
    * adding some logging to help troubleshoot
    
    * correcting bug in TLS config
    
    * async behavior bug
    
    * passcode forces a domain part on the username
    
    * adding conditional compilation to include LDAP provider
    
    * typos and clarifications
    
    * options validations and warnings
    
    * cleaning up and delcaring latest lang ver to fix ci issues
    
    * careless mistake

---
+++ src/Zyborg.PassCore.PasswordProvider.LDAP/LdapPasswordChangeOptions.cs
@@ -0,0 +1,49 @@
+using Novell.Directory.Ldap;
+
+namespace Zyborg.PassCore.PasswordProvider.LDAP
+{
+    public class LdapPasswordChangeOptions
+    {
+        /// Required, one or more hostnames or IP addresses which expose an LDAP/LDAPS
+        /// service endpoint that will be connected to.  If more than one host is
+        /// specified, then each will be tried in turn until a successful, secure
+        /// connection is established.
+        public string[] LdapHostnames { get; set; }
+
+        /// Optional, defaults to 636 -- the default port for LDAPS (i.e. LDAP over TLS).
+        /// A common alternative is to use the default LDAP port, 389, however this port
+        /// typically is not-secured and requires the "StartTLS" flag enabled.
+        public int LdapPort { get; set; } = LdapConnection.DEFAULT_SSL_PORT;
+
+        /// Optional, if 'true', then the specified port is a non-secured port by default
+        /// and requires the use of the "StartTLS" command over LDAP to enable TLS.
+        public bool LdapStartTls { get; set; } = false;
+
+        /// Optional, if 'true', then server certificates will be ignored for expiration
+        /// or common name mismatch.  Note this is a SUPERSET of the LdapIgnoreTlsValidation
+        /// options, so you don't have to set both.
+        public bool LdapIgnoreTlsErrors { get; set; } = false;
+
+        /// Optional, if 'true', then server certificates will be accepted regardless
+        /// of being signed by a trusted CA or intermediary (e.g. self-signed).
+        public bool LdapIgnoreTlsValidation { get; set; } = false;
+
+        /// The distinguished name (DN) of the privileged user account in the
+        /// target directory with permission to reset user passwords.
+        public string LdapBindUserDN { get; set; }
+
+        /// The password of the privileged user account in the
+        /// target directory with permission to reset user passwords.
+        public string LdapBindPassword { get; set; }
+
+        /// Distinguished Name (DN) of the base OU from which to search for
+        /// the target users by their username (SAM Account Name)
+        public string LdapSearchBase { get; set; }
+
+        /// When the user cannot be located in the directory, you can
+        /// either expose that error, or hide it and treat like an arbitrary
+        /// bad credential -- in order to prevent brute force attack to
+        /// discover the presence or absence of a username.
+        public bool HideUserNotFound { get; set; } = true;
+    }
+}
---
+++ src/Zyborg.PassCore.PasswordProvider.LDAP/LdapPasswordChangeProvider.cs
@@ -0,0 +1,307 @@
+﻿using System;
+using Microsoft.Extensions.Options;
+using Novell.Directory.Ldap;
+using LdapRemoteCertificateValidationCallback =
+        Novell.Directory.Ldap.RemoteCertificateValidationCallback;
+using Unosquare.PassCore.Common;
+using System.Security.Cryptography.X509Certificates;
+using System.Net.Security;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Globalization;
+using Microsoft.Extensions.Logging;
+
+namespace Zyborg.PassCore.PasswordProvider.LDAP
+{
+    public class LdapPasswordChangeProvider : IPasswordChangeProvider
+    {
+        private ILogger _logger;
+        private LdapPasswordChangeOptions _options;
+        private LdapRemoteCertificateValidationCallback _ldapRemoteCertValidator = null;
+
+        public LdapPasswordChangeProvider(ILogger<LdapPasswordChangeProvider> logger,
+            IOptions<LdapPasswordChangeOptions> options)
+        {
+            _logger = logger;
+            _options = options?.Value;
+
+            Init();
+        }
+
+        private void Init()
+        {
+            if (_options.LdapIgnoreTlsErrors || _options.LdapIgnoreTlsValidation)
+                _ldapRemoteCertValidator = CustomServerCertValidation;
+
+            // Validate required options
+
+            if (_options == null)
+                throw new Exception("missing configuration options");
+
+            if (_options.LdapHostnames?.Count() < 1)
+                throw new ArgumentException("options must specify at least one LDAP hostname",
+                        nameof(_options.LdapHostnames));
+            if (string.IsNullOrEmpty(_options.LdapBindUserDN))
+                throw new ArgumentException("options missing or invalid LDAP bind distinguished name (DN)",
+                        nameof(_options.LdapBindUserDN));
+            if (string.IsNullOrEmpty(_options.LdapBindPassword))
+                throw new ArgumentException("options missing or invalid LDAP bind password",
+                        nameof(_options.LdapBindPassword));
+            if (string.IsNullOrEmpty(_options.LdapSearchBase))
+                throw new ArgumentException($"options must specify LDAP search base",
+                        nameof(_options.LdapSearchBase));
+
+            // All other configuration is optional, but some may warrant attention
+
+            if (!_options.HideUserNotFound)
+                _logger.LogWarning($"option [{nameof(_options.HideUserNotFound)}] is DISABLED;"
+                        + " the presence or absence of usernames can be harvested");
+
+            if (!_options.LdapIgnoreTlsErrors)
+                _logger.LogWarning($"option [{nameof(_options.LdapIgnoreTlsErrors)}] is ENABLED;"
+                        + " invalid certificates will be allowed");
+            else if (!_options.LdapIgnoreTlsValidation)
+                _logger.LogWarning($"option [{nameof(_options.LdapIgnoreTlsValidation)}] is ENABLED;"
+                        + " untrusted certificate roots will be allowed");
+
+            if (_options.LdapPort != LdapConnection.DEFAULT_SSL_PORT && !_options.LdapStartTls)
+                _logger.LogWarning($"option [{nameof(_options.LdapStartTls)}] is DISABLED"
+                        + $" in combination with non-standard TLS port [{_options.LdapPort}]");
+        }
+
+        public ApiErrorItem PerformPasswordChange(string username,
+                string currentPassword, string newPassword)
+        {
+            var cleanUsername = username;
+
+            var atindex = cleanUsername.IndexOf("@");
+            if (atindex >= 0)
+                cleanUsername = cleanUsername.Substring(0, atindex);
+
+            // Must sanitize the username to eliminate the possibility of injection attacks:
+            //    * https://docs.microsoft.com/en-us/windows/desktop/adschema/a-samaccountname
+            //    * https://docs.microsoft.com/en-us/previous-versions/windows/it-pro/windows-2000-server/bb726984(v=technet.10)
+            var samInvalid = "\"/\\[]:;|=,+*?<>";
+            var miscInvalid = "\r\n\t";
+            var invalid = (samInvalid + miscInvalid).ToCharArray();
+            var invalidIndex = cleanUsername.IndexOfAny(invalid);
+            if (invalidIndex >= 0)
+            {
+                var msg = "username contains one or more invalid characters";
+                _logger.LogWarning(msg);
+                return new ApiErrorItem
+                {
+                    ErrorCode = ApiErrorCode.InvalidCredentials,
+                    FieldName = nameof(username),
+                    Message = msg,
+                };
+            }
+            
+            // LDAP filters require escaping of some special chars:
+            //    * http://www.ldapexplorer.com/en/manual/109010000-ldap-filter-syntax.htm
+            var escape = "()&|=><!*/\\".ToCharArray();
+            var escapeIndex = cleanUsername.IndexOfAny(escape);
+            if (escapeIndex >= 0)
+            {
+                var buff = new StringBuilder();
+                var maxLen = cleanUsername.Length;
+                var copyFrom = 0;
+                while (escapeIndex >= 0)
+                {
+                    buff.Append(cleanUsername.Substring(copyFrom, escapeIndex));
+                    buff.Append(string.Format("\\{0:X}", (int)cleanUsername[escapeIndex]));
+                    copyFrom = escapeIndex + 1;
+                    escapeIndex = cleanUsername.IndexOfAny(escape, copyFrom);
+                }
+                if (copyFrom < maxLen)
+                    buff.Append(cleanUsername.Substring(copyFrom));
+                cleanUsername = buff.ToString();
+                _logger.LogWarning("had to clean username: [{0}] => [{1}]", username, cleanUsername);
+            }
+
+            // Based on:
+            //    * https://www.cs.bham.ac.uk/~smp/resources/ad-passwds/
+            //    * https://support.microsoft.com/en-us/help/269190/how-to-change-a-windows-active-directory-and-lds-user-password-through
+            //    * https://ltb-project.org/documentation/self-service-password/latest/config_ldap#active_directory
+            //    * https://technet.microsoft.com/en-us/library/ff848710.aspx?f=255&MSPPError=-2147217396
+
+            using (var ldap = BindToLdap())
+            {
+                // First find user DN by username (SAM Account Name)
+                var searchConstraints = new LdapSearchConstraints(
+                        0, 0, LdapSearchConstraints.DEREF_NEVER,
+                        1000, true, 1, null, 10);
+                
+                var searchFilter = $"(sAMAccountName={cleanUsername})";
+                var search = ldap.Search(
+                        _options.LdapSearchBase, LdapConnection.SCOPE_SUB,
+                        searchFilter, new[] { "distinguishedName" },
+                        false, searchConstraints);
+
+                // We cannot use search.Count here -- apparently it does not
+                // wait for the results to return before resolving the count
+                // but fortunately hasMore seems to block until final result
+                if (!search.hasMore())
+                {
+                    _logger.LogWarning("unable to find username: [{0}]", cleanUsername);
+                    if (_options.HideUserNotFound)
+                    {
+                        return new ApiErrorItem
+                        {
+                            ErrorCode = ApiErrorCode.InvalidCredentials,
+                            FieldName = nameof(currentPassword),
+                            Message = "invalid credentials",
+                        };
+                    }
+                    else
+                    {
+                        return new ApiErrorItem
+                        {
+                            ErrorCode = ApiErrorCode.UserNotFound,
+                            FieldName = nameof(username),
+                            Message = "username could not be located",
+                        };
+                    }
+                }
+
+                if (search.Count > 1)
+                {
+                    _logger.LogWarning("found multiple with same username: [{0}]", cleanUsername);
+                    // Hopefully this should not ever happen if AD is preserving SAM Account Name
+                    // uniqueness constraint, but just in case, handling this corner case
+                    return new ApiErrorItem
+                    {
+                        ErrorCode = ApiErrorCode.UserNotFound,
+                        FieldName = nameof(username),
+                        Message = "multiple matching user entries resolved",
+                    };
+                }
+
+                var userDN = search.next().DN;
+
+                var oldPassBytes = Encoding.Unicode.GetBytes($@"""{currentPassword}""")
+                        .Select(x => (sbyte)x).ToArray();
+                var newPassBytes = Encoding.Unicode.GetBytes($@"""{newPassword}""")
+                        .Select(x => (sbyte)x).ToArray();
+                
+                var oldAttr = new LdapAttribute("unicodePwd", oldPassBytes);
+                var newAttr = new LdapAttribute("unicodePwd", newPassBytes);
+
+                var ldapDel = new LdapModification(LdapModification.DELETE, oldAttr);
+                var ldapAdd = new LdapModification(LdapModification.ADD, newAttr);
+
+                try
+                {
+                    ldap.Modify(userDN, new[] { ldapDel, ldapAdd });
+                }
+                catch (LdapException ex)
+                {
+                    _logger.LogWarning("failed to update password", ex);
+                    return ParseLdapException(ex);
+                }
+
+                ldap.StopTls();
+                ldap.Disconnect();
+            }
+
+            // Everything seems to have worked:
+            return null;
+        }
+
+        private ApiErrorItem ParseLdapException(LdapException ex)
+        {
+            // If the LDAP server returned an error, it will be formated
+            // similar to this:
+            //    "0000052D: AtrErr: DSID-03191083, #1:\n\t0: 0000052D: DSID-03191083, problem 1005 (CONSTRAINT_ATT_TYPE), data 0, Att 9005a (unicodePwd)\n\0"
+            //
+            // The leading number before the ':' is the Win32 API Error Code in HEX
+            var m = Regex.Match(ex.LdapErrorMessage, "([0-9a-fA-F]+):");
+            if (m.Success)
+            {
+                var errCodeString = m.Groups[1].Value;
+                var errCode = int.Parse(errCodeString, NumberStyles.HexNumber);
+                var err = Win32ErrorCode.ByCode(errCode);
+
+                if (err != null)
+                {
+                    _logger.LogWarning("resolved Win32 API Error: code={0} name={1} desc={2}",
+                            err.Code, err.CodeName, err.Description);
+                    return new ApiErrorItem
+                    {
+                        ErrorCode = ApiErrorCode.InvalidCredentials,
+                        FieldName = "currentPassword",
+                        Message = $"0x{err.Code:X}:{err.CodeName}: {err.Description}",
+                    };
+                }
+
+                return new ApiErrorItem
+                {
+                    ErrorCode = ApiErrorCode.Generic,
+                    Message = "unexpected Win32 API error; error code: " + errCodeString,
+                };
+            }
+            return new ApiErrorItem
+            {
+                ErrorCode = ApiErrorCode.Generic,
+                Message = "unexpected error: " + ex.LdapErrorMessage,
+            };
+        }
+
+        private LdapConnection BindToLdap()
+        {
+            var ldap = new LdapConnection();
+            if (_ldapRemoteCertValidator != null)
+                ldap.UserDefinedServerCertValidationDelegate += _ldapRemoteCertValidator;
+
+            if (!_options.LdapStartTls)
+                ldap.SecureSocketLayer = true;
+
+            string bindHostname = null;
+            foreach (var h in _options.LdapHostnames)
+            {
+                try
+                {
+                    ldap.Connect(h, _options.LdapPort);
+                    bindHostname = h;
+                    break;
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogWarning($"failed to connect to host [{h}]", ex);
+                }
+            }
+
+            if (string.IsNullOrEmpty(bindHostname))
+                throw new Exception("failed to connect to any configured hostname");
+            
+            if (_options.LdapStartTls)
+                ldap.StartTls();
+            
+            ldap.Bind(_options.LdapBindUserDN, _options.LdapBindPassword);
+
+            return ldap;            
+        }
+
+        /// Custom server certificate validation logic that handles our special
+        /// cases based on configuration.  This implements the logic of either
+        /// ignoring just untrusted root errors or ignoring all TLS errors.
+        private bool CustomServerCertValidation(object sender, X509Certificate certificate,
+                X509Chain chain, SslPolicyErrors sslPolicyErrors)
+        {
+            if (_options.LdapIgnoreTlsErrors || sslPolicyErrors == SslPolicyErrors.None)
+                return true;
+
+            var errorStatuses = chain.ChainStatus.Select((x, y) => (status: x, index: y)).Where(x => {
+                if (x.status.Status == X509ChainStatusFlags.UntrustedRoot
+                        && _options.LdapIgnoreTlsValidation)
+                    return false;
+                if (x.status.Status == X509ChainStatusFlags.NoError)
+                    return false;
+                return true;
+            }).ToArray();
+
+            return errorStatuses.Length > 0;
+        }
+    }
+}
