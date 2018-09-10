74a21f31028bdbd8cff653b729d6100079bdfaa3
Tjark <diogenes25@users.noreply.github.com>
2018-09-10 14:49:19 +0200

    Bugfix ldap provider (#208)
    * Added new section 'LdapPasswordChangeOptions' in appsettings.json file, to use the class LdapPasswordChangeOptions correctly
    
    * Suggestion for changing the password by replacing the password-attribute-value
    
    * Another SearchFilter if Attribute sAMAccountName is not available and use the 'cn'-Attribute instead.
    
    * Added internal processing exception containing ApiErrorItem information.
    
    Processing information was added to method LdapPasswordChangeProvider.PerformPasswordChange.
    Errors connecting to the LDAP server are now displayed on the web page.
    Fixes https://github.com/unosquare/passcore/issues/207#issue-357947362
    
    * Added functionality to define a Ldap-Search-String and how the password change is done technically.
    
    The change could be done by Del/Add password or by just replacing this attribute.
    
    Some changes due to Codacy / PR Quality Review
    - Removed the unused fields.
    - Removed redundant parentheses.
    
    Removed some lines in appsettings in Ldap-Section.
    
    * Bugfix with username used in SearchFilter.
    
    * Some changes due to Codacy / PR Quality Review regarding general exceptions.

---
+++ src/Unosquare.PassCore.Common/ApiErrorException.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Unosquare.PassCore.Common
+{
+    /// <summary>
+    /// Special Exception to transport the ApiErrorItem.
+    /// </summary>
+    public class ApiErrorException : Exception
+    {
+        public ApiErrorItem ErrorItem { get; set; }
+
+        public ApiErrorException() 
+        {
+            // Default ApiErrorItem to prevent Null-PointerException
+            ErrorItem = new ApiErrorItem
+            {
+                ErrorCode = ApiErrorCode.Generic,
+                FieldName = "",
+                Message = "Some Error"
+            };
+        }
+
+        /// <summary>
+        /// Exception message and/or ApiErrorItem.Message.
+        /// </summary>
+        public override string Message
+        {
+            get
+            {
+                string mess = base.Message;
+
+                if (this.ErrorItem != null)
+                {
+                    if (!String.IsNullOrWhiteSpace(base.Message))
+                    {
+                        mess += ": ";
+                    }
+                    mess += this.ErrorItem.Message;
+                }
+                return mess;
+            }
+        }
+    }
+}
+
+
