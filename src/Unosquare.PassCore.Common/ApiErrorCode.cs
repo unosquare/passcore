bb0658da34e646b9cdeeffdaa07def24434dfff1
Geo Perez <geovanni.perez@gmail.com>
2018-10-10 11:13:29 -0500

    Move ErrorCode to a single file

---
+++ src/Unosquare.PassCore.Common/ApiErrorCode.cs
@@ -0,0 +1,17 @@
+﻿namespace Unosquare.PassCore.Common
+{
+    /// <summary>
+    /// Represents error codes
+    /// </summary>
+    public enum ApiErrorCode
+    {
+        Generic = 0,
+        FieldRequired = 1,
+        FieldMismatch = 2,
+        UserNotFound = 3,
+        InvalidCredentials = 4,
+        InvalidCaptcha = 5,
+        ChangeNotPermitted = 6,
+        InvalidDomain = 7
+    }
+}
--- src/Unosquare.PassCore.Common/ApiErrorItem.cs
+++ src/Unosquare.PassCore.Common/ApiErrorItem.cs
@@ -1,21 +1,6 @@
 ﻿namespace Unosquare.PassCore.Common
 {
     /// <summary>
-    /// Represents error codes
-    /// </summary>
-    public enum ApiErrorCode
-    {
-        Generic = 0,
-        FieldRequired = 1,
-        FieldMismatch = 2,
-        UserNotFound = 3,
-        InvalidCredentials = 4,
-        InvalidCaptcha = 5,
-        ChangeNotPermitted = 6,
-        InvalidDomain = 7
-    }
-
-    /// <summary>
     /// Defines the fields contained in one of the items of Api Errors
     /// </summary>
     public class ApiErrorItem
