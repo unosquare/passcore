766dacba0f85c7da6b66f4b56c154cc8ab4dcc68
Thomas Grainger <tagrain@gmail.com>
2019-04-08 11:05:41 +0100

    use zxcvbn for password strength calculation

--- src/Unosquare.PassCore.Web/ClientApp/app/helpers/passwordStrength.ts
+++ src/Unosquare.PassCore.Web/ClientApp/app/helpers/passwordStrength.ts
@@ -1,33 +1,7 @@
-export class PasswordStrength{
-    
+import * as zxcvbn from 'zxcvbn';
+
+export class PasswordStrength {
     static measureStrength(p: string) {
-        let force = 0;
-        const regex = /[$-/:-?{-~!"^_`\[\]]/g;
-
-        const lowerLetters = /[a-z]+/.test(p);
-        const upperLetters = /[A-Z]+/.test(p);
-        const numbers = /[0-9]+/.test(p);
-        const symbols = regex.test(p);
-
-        const flags = [lowerLetters, upperLetters, numbers, symbols];
-
-        let passedMatches = 0;
-        for (let flag of flags) {
-            passedMatches += flag ? 1 : 0;
-        }
-
-        force += 2 * p.length + ((p.length >= 10) ? 1 : 0);
-        force += passedMatches * 10;
-
-        // penalty (short password)
-        force = (p.length <= 6) ? Math.min(force, 10) : force;
-
-        // penalty (poor variety of characters)
-        force = (passedMatches === 1) ? Math.min(force, 10) : force;
-        force = (passedMatches === 2) ? Math.min(force, 20) : force;
-        force = (passedMatches === 3) ? Math.min(force, 40) : force;
-
-        return force;
+        return Math.min(zxcvbn(p).guesses_log10 * 10, 100);
     }
-
 }
