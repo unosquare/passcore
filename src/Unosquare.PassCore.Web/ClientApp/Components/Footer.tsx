d171782686c764aef2d794f57c7dc03927f2a60c
Ramiro Flores <Serk352@gmail.com>
2019-08-08 18:11:15 -0500

    Footer
    Start working on footer.

---
+++ src/Unosquare.PassCore.Web/ClientApp/Components/Footer.tsx
@@ -0,0 +1,17 @@
+import *  as React from 'react';
+import Grid from '@material-ui/core/Grid/Grid';
+
+const Footer: React.FunctionComponent<any> = () => {
+
+    return (
+        <React.Fragment>
+            <Grid
+                container={true}
+                direction='row'
+            >
+                {/* for images */}
+            </Grid>
+            {/* here put the license text */}
+        </React.Fragment>
+    );
+};
