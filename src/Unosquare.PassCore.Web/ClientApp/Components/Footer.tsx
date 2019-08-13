471561cd5e7e028b5ded138f578a1b9edc04ec49
Geovanni Perez <1775792+geoperez@users.noreply.github.com>
2019-08-13 09:27:32 -0500

    Update Footer.tsx

--- src/Unosquare.PassCore.Web/ClientApp/Components/Footer.tsx
+++ src/Unosquare.PassCore.Web/ClientApp/Components/Footer.tsx
@@ -44,7 +44,7 @@ export const Footer: React.FunctionComponent<any> = () => (
                 align='center'
                 variant='caption'
             >
-                Powered by PassCore v3.6.4 - Open Source Initiative and MIT Licensed
+                Powered by PassCore v4.0.0 - Open Source Initiative and MIT Licensed
             </Typography>
             <Typography
                 align='center'
