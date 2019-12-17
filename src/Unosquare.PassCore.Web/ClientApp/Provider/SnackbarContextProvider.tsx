44d0b1d9dd68c645090288d20bc1e1a4adbbe312
Manuel Santos <manusanram@gmail.com>
2019-12-17 16:44:47 -0600

    Updated snackbar

--- src/Unosquare.PassCore.Web/ClientApp/Provider/SnackbarContextProvider.tsx
+++ src/Unosquare.PassCore.Web/ClientApp/Provider/SnackbarContextProvider.tsx
@@ -1,25 +1,16 @@
 import * as React from 'react';
-import { GlobalSnackbar } from 'uno-material-ui';
+import { SnackbarContainer, snackbarService } from 'uno-material-ui';
 import { SnackbarContext } from './GlobalContext';
 
-export const SnackbarContextProvider: React.FunctionComponent = ({ children }) => {
-    const [getMessage, setMessage] = React.useState({
-        messageText: '',
-        messageType: 'success',
-    });
-
-    const [providerValue] = React.useState({
+export const SnackbarContextProvider: React.FunctionComponent = ({ children }) => {const [providerValue] = React.useState({
         sendMessage: (messageText: string, messageType = 'success') =>
-            setMessage({
-                messageText,
-                messageType,
-            }),
+        snackbarService.showSnackbar(messageText, messageType),
     });
 
     return (
         <SnackbarContext.Provider value={providerValue}>
             {children}
-            <GlobalSnackbar seconds={5000} message={getMessage} mobile={false} />
+            <SnackbarContainer />
         </SnackbarContext.Provider>
     );
 };
