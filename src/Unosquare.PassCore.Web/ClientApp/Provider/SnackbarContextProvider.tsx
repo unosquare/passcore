09f9c52b918268b914490002ff39dd301cbb8309
Geo Perez <1775792+geoperez@users.noreply.github.com>
2019-12-18 10:02:27 -0600

    New workflow

--- src/Unosquare.PassCore.Web/ClientApp/Provider/SnackbarContextProvider.tsx
+++ src/Unosquare.PassCore.Web/ClientApp/Provider/SnackbarContextProvider.tsx
@@ -2,9 +2,10 @@ import * as React from 'react';
 import { SnackbarContainer, snackbarService } from 'uno-material-ui';
 import { SnackbarContext } from './GlobalContext';
 
-export const SnackbarContextProvider: React.FunctionComponent = ({ children }) => {const [providerValue] = React.useState({
+export const SnackbarContextProvider: React.FunctionComponent = ({ children }) => {
+    const [providerValue] = React.useState({
         sendMessage: (messageText: string, messageType = 'success') =>
-        snackbarService.showSnackbar(messageText, messageType),
+            snackbarService.showSnackbar(messageText, messageType),
     });
 
     return (
